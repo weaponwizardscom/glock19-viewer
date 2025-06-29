508a1e84fac8d71430d1e010ac84cd8b98c7893f
weaponwizardscom <contact@weapon-wizards.com>
2025-06-29 22:25:15 +0200

    1

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,26 @@
-import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
-import path from 'path'
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   base: '/glock19-viewer/',
+  build: { outDir: 'dist' },
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
+      includeAssets: ['img/**', 'manifest/**', 'favicon.svg'],
       manifest: {
         name: 'Glock Viewer',
-        short_name: 'Viewer',
+        short_name: 'G19 Viewer',
         start_url: '/glock19-viewer/',
+        scope: '/glock19-viewer/',
         display: 'standalone',
         background_color: '#000000',
         theme_color: '#000000',
         icons: [
-          {
-            src: 'icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
+          { src: 'manifest/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'manifest/icon-512.png', sizes: '512x512', type: 'image/png' }
         ]
       }
     })
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './')
-    }
-  }
-})
+  ]
+});
