a57a9d9a287a794cc1854216a7803059393101bb
weaponwizardscom <contact@weapon-wizards.com>
2025-06-29 22:44:59 +0200

    1

---
+++ src/data/config.js
@@ -0,0 +1,22 @@
+// src/data/config.js
+// wszystkie stałe przeniesione z app.js
+export const SVG_FILE  = "g17.svg";
+export const TEXTURE   = "img/glock17.png";
+export const BG        = [
+  "img/t1.png","img/t2.png","img/t3.png","img/t4.png",
+  "img/t5.png","img/t6.png","img/t7.png"
+];
+
+export const PRICE = {
+  zamek:    400,
+  szkielet: 400,
+  spust:    150,
+  lufa:     200,
+  zerdz:     50,
+  pazur:     50,
+  zrzut:     50,
+  blokadap:  50,
+  blokada2:  50,
+  pin:       50,
+  stopka:   150          // płytka = 0
+};
