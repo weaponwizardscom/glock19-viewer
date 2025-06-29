c3fcbb08122c3cdab1ddf8482bbede03a184217d
weaponwizardscom <contact@weapon-wizards.com>
2025-06-29 22:21:28 +0200

    1

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,37 @@
-import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+import path from 'path'
 
 export default defineConfig({
   base: '/glock19-viewer/',
-  build: { outDir: 'dist' },
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['img/**/*', 'manifest/**/*', 'favicon.svg'],
       manifest: {
-        name: 'Glock 19 Configurator',
-        short_name: 'G19 Viewer',
+        name: 'Glock Viewer',
+        short_name: 'Viewer',
         start_url: '/glock19-viewer/',
-        scope: '/glock19-viewer/',
         display: 'standalone',
         background_color: '#000000',
         theme_color: '#000000',
         icons: [
-          { src: '/glock19-viewer/manifest/icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/glock19-viewer/manifest/icon-512.png', sizes: '512x512', type: 'image/png' }
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
         ]
       }
     })
-  ]
-});
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './')
+    }
+  }
+})
